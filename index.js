01be5b82bd579f49b9e344f2200800705d9010d3
Samir-Route <ahmed.samir.route@gmail.com>
2025-07-24 22:22:38 +0300

    3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import bootstrap from './src/app.controller.js';
 
 //hi
 //hi again
-
+// Load environment variables
 dotenv.config({path:path.resolve('./src/config/.env')});
 
 const app = express();
